30ee8a41554ed26a979a2bf9955641f243eefa19
unknown <nray24@gmail.com>
2017-12-22 15:17:44 -0700

    15.17

--- assets/javascript/nyt-search.js
+++ assets/javascript/nyt-search.js
@@ -1,4 +1,22 @@
-// Built by LucyBot. www.lucybot.com
+// SETUP VARIABLES
+// ============================================
+var authKey = "c4d0935b7d4d411586a4d5453386e817";
+
+// FUNCTIONS
+// ============================================
+
+// MAIN PROCESSES
+// ============================================
+
+// 1. Retrieve user inputs and convert to variables
+// 2. Use those variables to run an AJAX to the New York Times
+// 3. Break down the NYT Object into useable fields
+// 4. Dynamically generate html content
+
+// 5. Dealing with "edge cases" -- bugs or situations that are not intuitive.
+
+
+/*// Built by LucyBot. www.lucybot.com
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "c4d0935b7d4d411586a4d5453386e817",
@@ -11,4 +29,4 @@ $.ajax({
   console.log(result);
 }).fail(function(err) {
   throw err;
-});
+});*/
