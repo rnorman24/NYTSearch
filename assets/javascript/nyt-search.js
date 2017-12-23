f5c2f4b424467b6e695e6bb083499fb9647e1b64
unknown <nray24@gmail.com>
2017-12-22 18:42:00 -0700

    18.41

--- assets/javascript/nyt-search.js
+++ assets/javascript/nyt-search.js
@@ -17,37 +17,86 @@ var articleCounter = 0;
 // FUNCTIONS
 // ============================================
 function runQuery(numArticles, queryURL) {
-
+console.log(queryURL);
   // AJAX Function
   $.ajax({url: queryURL, method: "GET"})
     .done(function(NYTData) {
+
+      // Clear the wells from the previous run
+      $("#results").empty();
+
+      for (var i = 0; i < NYTData.response.docs.length; i++) {
+
+      // Start Dumping to HTML Here
+      var results = $("<div>");
+      results.addClass("result");
+      results.attr("id", "articleResult-" + i);
+      $("#results").append(results);
+
+      // Check if things exist
+      if (NYTData.response.docs[i].headline != "null") {
+        $("#articleResult-" + i).append("<h3>" + NYTData.response.docs[i].headline.main + "</h3>");
+      }
+
+      // Check if the byline
+      if (NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.hasWonProperty("original")){
+        $("#articleResult-" + i).append("<h5>" + NYTData.response.docs[i].byline.original + "</h5>");
+      }
+
+      // Attach the content to the appropriate well
+      $("#articleResult-" + i).append("<h5>" + NYTData.response.docs[i].section_name + "</h5>");
+      $("#articleResult-" + i).append("<h5>" + NYTData.response.docs[i].pub_date + "</h5>");
+      $("#articleResult-" + i).append("<a href=" + NYTData.response.docs[i].web_url + ">" + NYTData.response.docs[i].web_url + "</a>");
+
+      }
+      console.log(queryURL);
+      console.log(numArticles);
       console.log(NYTData);
     })
 }
 // MAIN PROCESSES
 // ============================================
-$("#search-btn").on("click", function() {
-  runQuery();
+$("#searchBtn").on("click", function() {
+
+  queryTerm = $("#search").val().trim();
+
+  // Add in the Search Term
+  var newURL = queryURLBase + "&q" + queryTerm;
+
+  // Get the Number of Records
+  numResults = $("#num-records").val();
+
+  // Get the Start Year and End Year
+  startYear = $("#start-year").val().trim();
+  endYear = $("#end-year").val().trim();
+
+  if (parseInt(startYear)) {
+
+    // Add the vecessary fields
+    startYear = startYear + "0101";
+
+    // Add the date information to the URL
+    newURL = newURL + "&begin_date=" + startYear;
+  }
+
+  if (parseInt(endYear)) {
+
+    // Add the necessary fields
+    endYear = endYear + "0101";
+
+    // Add the date information to the URL
+    newURL = newURL + "&end_date=" + endYear;
+  }
+
+  // Send the AJAX Call the newly asswmbled URL
+  runQuery(numResults, newURL);
+
+  return false;
 })
+
 // 1. Retrieve user inputs and convert to variables
 // 2. Use those variables to run an AJAX to the New York Times
 // 3. Break down the NYT Object into useable fields
 // 4. Dynamically generate html content
 
 // 5. Dealing with "edge cases" -- bugs or situations that are not intuitive.
-
-
-/*// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "c4d0935b7d4d411586a4d5453386e817",
-  'q': "Obama"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});*/
